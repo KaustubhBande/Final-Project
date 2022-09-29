e5495b509b94f2e5a4337f77c4625a7a24bb730e
Kaustubh Bande <kaustubhdb96@gmail.com>
2022-09-29 15:20:59 +0530

    commit5

--- src/pages/Adminhome.jsx
+++ src/pages/Adminhome.jsx
@@ -3,9 +3,20 @@ import React from 'react';
 const Adminhome = () => {
     return (
         <div>
-            <h1>Admin Home</h1>
+            <img src="https://th.bing.com/th/id/R.04dabe433e00ce2ea6aaf0ff68d75c3e?rik=60HDDQ35jSVH8w&riu=http%3a%2f%2fgetwallpapers.com%2fwallpaper%2ffull%2f9%2f6%2f4%2f856321-best-goal-wallpapers-1920x1200-download.jpg&ehk=%2fYGj2kRzJzDD4o5KxAF5U5m2fgJB5E8FHeniHeMFlXA%3d&risl=&pid=ImgRaw&r=0" style={styles.container} />
+
         </div>
     );
 };
 
 export default Adminhome;
+
+const styles = {
+    container: {
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        width: '100vw',
+        height: '100vh'
+    }
+}
--- src/pages/ManagerHome.jsx
+++ src/pages/ManagerHome.jsx
@@ -3,9 +3,20 @@ import React from 'react';
 const ManagerHome = () => {
     return (
         <div>
-            <h1>Manager Home</h1>
+            <img src="https://wallpaperaccess.com/full/2135217.jpg" style={styles.container} />
+
         </div>
     );
 };
 
 export default ManagerHome;
+
+const styles = {
+    container: {
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        width: '100vw',
+        height: '100vh'
+    }
+}
